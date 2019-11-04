3f503cea2f93e0c744b363497bf82d9c97448431
Joel Thayer <joelthayer2345@hotmail.com>
2019-11-03 22:13:05 -0700

    push

---
+++ Homepage1/sketch.js
@@ -0,0 +1,21 @@
+var redColor = 110;
+var greenColor = 100;
+var blueColor = 190;
+
+var x 100;
+var y = 200;
+var diameter = 50;
+  // this function is called upon once
+function setup()
+{
+createCanvas(800,600);
+}
+/* this is called upon continuously in an open browser
+*/
+function draw()
+{
+background(redColor,greenColor,blueColor);
+circle(x,y,diameter);
+x++;
+
+}
